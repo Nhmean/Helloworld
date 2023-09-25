45dd352992ca4f2ca83ae55f1f0cc8c5efa976e5
Nhmean <145417908+Nhmean@users.noreply.github.com>
2023-09-25 10:11:05 +0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,43 @@
+<div class="cart">
+        <div class="cart-title">Cart Items</div>
+        <div class="cart-content">
+
+          <div class="cart-box">
+            <img src="image/6.jpg" class="cart-img">
+            <div class="detail-box">
+              <div class="cart-food-title">Special Dosai</div>
+              <div class="price-box">
+                <div class="cart-price">10$</div>
+                 <div class="cart-amt">10$</div>
+             </div>
+              <input type="number" value="1" class="cart-quantity">
+            </div>
+            <ion-icon name="trash" class="cart-remove"></ion-icon>
+          </div>
+
+          <div class="cart-box">
+            <img src="image/11.jpg" class="cart-img">
+            <div class="detail-box">
+              <div class="cart-food-title">Special Dosai</div>
+              <div class="price-box">
+                <div class="cart-price">10$</div>
+                 <div class="cart-amt">10$</div>
+             </div>
+              <input type="number" value="1" class="cart-quantity">
+            </div>
+            <ion-icon name="trash" class="cart-remove"></ion-icon>
+          </div>
+        
+        </div>
+        
+      <div class="total">
+        <div class="total-title">Total</div>
+        <div class="total-price">$</div>
+      </div>
+
+      <button class="btn-buy">Place Order</button>
+
+      <ion-icon name="close" id="cart-close"></ion-icon>
+
+      </div>
+    </div>
